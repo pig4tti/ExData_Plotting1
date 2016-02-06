120e5e5d786ae919d8bb463f5e05835959acc5ea
Rodrigo Pigatti Marroquim <pig4tti@gmail.com>
2016-02-06 15:08:35 -0300

    Remove extra lines

--- plot2.R
+++ plot2.R
@@ -7,5 +7,3 @@ png(file='plot2.png', width = 480, height = 480, units = 'px')
 plot(data$Global_active_power, type = 'l', xaxt = "n", ylab='Global Active Power (Kws)', xlab = "")
 axis(1, at = c(1, as.integer(nrow(data)/2), nrow(data)), labels = c("Thu", "Fri", "Sat"))
 dev.off()
-
-
