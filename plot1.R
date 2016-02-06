b3775d7720cc5106a1cf933fb75fc53ca22ccc8c
Rodrigo Pigatti Marroquim <pig4tti@gmail.com>
2016-02-06 11:46:51 -0300

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+source('HelperFunction.R')
+
+data <- getDataSet()
+
+# plot 1
+png(file='plot1.png', width = 480, height = 480, units = 'px')
+hist(data$Global_active_power, col='red', main='Global Active Power', xlab='Global Active Power (Kws)')
+dev.off()
