a7912b30c9e556ecde6c71452dfadfe48a231d08
Rodrigo Pigatti Marroquim <pig4tti@gmail.com>
2016-02-06 15:07:33 -0300

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+source('HelperFunction.R')
+
+data <- getDataSet()
+
+# plot 2
+png(file='plot2.png', width = 480, height = 480, units = 'px')
+plot(data$Global_active_power, type = 'l', xaxt = "n", ylab='Global Active Power (Kws)', xlab = "")
+axis(1, at = c(1, as.integer(nrow(data)/2), nrow(data)), labels = c("Thu", "Fri", "Sat"))
+dev.off()
+
+
